15aa50709813c97f3ac79abfef546cfc6b61aa69
dushyant <dushyant.977360@gmail.com>
2023-11-10 00:40:26 +0530

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.brorental.bropartner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.brorental.bropartner"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -25,6 +26,10 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        dataBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -39,4 +44,36 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //dots indicator
+    implementation("com.tbuonomo:dotsindicator:5.0")
+    //firebase libraries
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-auth")
+    //pinview
+    implementation("io.github.chaosleung:pinview:1.4.4")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.10")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+
+    //image slider
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.0")
+
+    //gif
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.25")
+
+    //shimmer
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    //circle image view
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    //swipe refresh layout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
 }
--- app/src/main/java/com/brorental/bropartner/MainActivity.java
+++ app/src/main/java/com/brorental/bropartner/MainActivity.java
@@ -1,14 +1,296 @@
 package com.brorental.bropartner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.brorental.bropartner.activities.HistoryActivity;
+import com.brorental.bropartner.activities.PaymentActivity;
+import com.brorental.bropartner.activities.PaymentHistory;
+import com.brorental.bropartner.activities.ProfileActivity;
+import com.brorental.bropartner.activities.RideActivity;
+import com.brorental.bropartner.adapters.RentListAdapter;
+import com.brorental.bropartner.databinding.ActivityMainBinding;
+import com.brorental.bropartner.fragments.SearchFragment;
+import com.brorental.bropartner.models.RentItemModel;
+import com.brorental.bropartner.utilities.AppClass;
+import com.brorental.bropartner.utilities.DialogCustoms;
+import com.brorental.bropartner.utilities.Utility;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private String TAG = "MainBinding.java";
+    private RentListAdapter adapter;
+    private ArrayList<RentItemModel> list = new ArrayList<>();
+    private FirebaseFirestore mFirestore;
+    private AppClass appClass;
+    private TextView headerWalletTV, viewProfileTV, headerNameTV;
+    private ImageView headerImageView;
+    private LinearLayout headerWalletLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        appClass = (AppClass) getApplication();
+        mFirestore = appClass.firestore;
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, 0, 0);
+        mDrawerToggle.syncState();
+        //After instantiating your ActionBarDrawerToggle
+        mDrawerToggle.setDrawerIndicatorEnabled(false);
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.navigation_menu_ic, getTheme());
+        mDrawerToggle.setHomeAsUpIndicator(drawable);
+        mDrawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    binding.drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        binding.drawerLayout.addDrawerListener(mDrawerToggle);
+
+        //header listeners and dynamic text.
+        View headerView = binding.navigationView.getHeaderView(0);
+        headerWalletTV = headerView.findViewById(R.id.walletTV);
+        headerImageView = headerView.findViewById(R.id.profileIV);
+        viewProfileTV = headerView.findViewById(R.id.viewProfileTV);
+        headerNameTV = headerView.findViewById(R.id.nameTV);
+        headerWalletLL = headerView.findViewById(R.id.walletLL);
+        headerWalletTV.setText("\u20B9 " + appClass.sharedPref.getUser().getWallet());
+        headerNameTV.setText(appClass.sharedPref.getUser().getName());
+
+        Glide.with(this).load(appClass.sharedPref.getUser().getProfileUrl()).placeholder(R.drawable.profile_24).into(headerImageView);
+
+        setListeners();
+        //REGISTER BROADCAST RECEIVER FOR INTERNET
+        Utility.registerConnectivityBR(MainActivity.this, appClass);
+        if (Utility.isNetworkAvailable(this)) {
+            getData("", "");
+        } else {
+            Snackbar bar = Snackbar.make(binding.getRoot(), "No Connection", Snackbar.LENGTH_INDEFINITE);
+            bar.setAction("Refresh", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Utility.isNetworkAvailable(MainActivity.this)) {
+                        getData("", "");
+                        bar.dismiss();
+                        Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+                    } else {
+                        bar.dismiss();
+                        bar.show();
+                    }
+                }
+            });
+
+            bar.show();
+        }
+
+        adapter = new RentListAdapter(this);
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        binding.recyclerView.setAdapter(adapter);
+        adapter.submitList(list);
+    }
+
+    private void setListeners() {
+        //header listeners and dynamic text.
+        View headerView = binding.navigationView.getHeaderView(0);
+        TextView walletTV = headerView.findViewById(R.id.walletTV);
+        ImageView imageView = headerView.findViewById(R.id.profileIV);
+        TextView viewProfileTV = headerView.findViewById(R.id.viewProfileTV);
+        TextView nameTV = headerView.findViewById(R.id.nameTV);
+        LinearLayout walletLL = headerView.findViewById(R.id.walletLL);
+        walletTV.setText("\u20B9 " + appClass.sharedPref.getUser().getWallet());
+        nameTV.setText(appClass.sharedPref.getUser().getName());
+
+        Glide.with(this).load(appClass.sharedPref.getUser().getProfileUrl()).placeholder(R.drawable.profile_24).into(imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivityForRes(i);
+            }
+        });
+
+        viewProfileTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivityForRes(i);
+            }
+        });
+
+        binding.addCashBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog sheet = new BottomSheetDialog(MainActivity.this);
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_cash_sheet, null);
+                sheet.setContentView(view);
+                Button submitBtn = view.findViewById(R.id.confirmRec);
+                Button cancelBtn = view.findViewById(R.id.cancelRec);
+                EditText rechargeET = view.findViewById(R.id.rechargeAmt);
+                submitBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this, PaymentActivity.class);
+                        i.putExtra("addCash", true);
+                        i.putExtra("amt", rechargeET.getText().toString());
+                        startActivityForRes(i);
+                        sheet.dismiss();
+                    }
+                });
+                cancelBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sheet.dismiss();
+                    }
+                });
+                sheet.show();
+            }
+        });
+        binding.searchLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFragment(new SearchFragment());
+            }
+        });
+
+        walletLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PaymentHistory.class);
+                startActivity(i);
+            }
+        });
+
+        binding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.profile) {
+                    Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                    startActivity(i);
+                } else if (id == R.id.rent) {
+                    binding.drawerLayout.close();
+                } else if (id == R.id.driving) {
+                    Intent i = new Intent(MainActivity.this, RideActivity.class);
+                    startActivity(i);
+                } else if (id == R.id.history) {
+                    Intent i = new Intent(MainActivity.this, HistoryActivity.class);
+                    startActivity(i);
+                } else if (id == R.id.termsCon) {
+                    DialogCustoms.showSnackBar(MainActivity.this, "Terms & Conditions", binding.getRoot());
+                }
+
+                return true;
+            }
+        });
+
+        binding.swipeRef.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData("", "");
+            }
+        });
+    }
+
+    public void getData(String selectedState, String category) {
+        Log.d(TAG, "getData: " + selectedState + "," + category);
+        binding.shimmer.setVisibility(View.VISIBLE);
+        binding.recyclerView.setVisibility(View.GONE);
+        Query query = mFirestore.collection("rent").limit(10);
+        if (!selectedState.isEmpty()) {
+            query = mFirestore.collection("rent").whereEqualTo("state", selectedState)
+                    .whereEqualTo("category", category).limit(10);
+        }
+        query.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        binding.swipeRef.setRefreshing(false);
+                        binding.shimmer.setVisibility(View.GONE);
+                        binding.recyclerView.setVisibility(View.VISIBLE);
+                        if (task.isSuccessful()) {
+                            list.clear();
+                            List<DocumentSnapshot> docList = task.getResult().getDocuments();
+                            for (int i = 0; i < docList.size(); i++) {
+                                DocumentSnapshot d = docList.get(i);
+                                RentItemModel model = d.toObject(RentItemModel.class);
+                                list.add(model);
+                            }
+
+                            adapter.notifyDataSetChanged();
+                            Log.d(TAG, "onComplete: " + docList.size());
+                        } else {
+                            Log.d(TAG, "onComplete: " + task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void openFragment(SearchFragment searchFragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fT = fm.beginTransaction();
+        fT.replace(R.id.fragmentContainer, searchFragment);
+        fT.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        fT.addToBackStack(null);
+        fT.commit();
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resCode, Intent intent) {
+        super.onActivityResult(reqCode, resCode, intent);
+        Log.d(TAG, "onActivityResult: 44");
+        switch (reqCode) {
+            case 101:
+                headerWalletTV.setText(Utility.rupeeIcon + appClass.sharedPref.getUser().getWallet());
+                headerNameTV.setText(appClass.sharedPref.getUser().getName());
+                Glide.with(this).load(appClass.sharedPref.getUser().getProfileUrl()).placeholder(R.drawable.profile_24).into(headerImageView);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void startActivityForRes(Intent i) {
+        if (!isFinishing())
+            startActivityForResult(i, 101);
     }
 }
