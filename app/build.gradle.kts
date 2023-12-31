fc1dae3e9c4e16a8c6705f1271ee905ce8f180f6
dushyant <dushyant.977360@gmail.com>
2023-12-31 16:28:01 +0530

    version 4(1.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.brorental.bropartner"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
